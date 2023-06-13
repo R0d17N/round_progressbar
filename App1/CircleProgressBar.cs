5534a7841c60acf42d8ea61bf3dd53426b35543e
R0d17N <79056898+R0d17N@users.noreply.github.com>
2023-06-13 21:17:23 +0300

    Add files via upload

---
+++ App1/CircleProgressBar.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public class CircleProgressBar : SKCanvasView
+    {
+        // Определение свойства прогресса
+        public static readonly BindableProperty ProgressProperty =
+            BindableProperty.Create(nameof(Progress), typeof(double), typeof(CircleProgressBar), 0d,
+                propertyChanged: OnProgressPropertyChanged);
+
+        public double Progress
+        {
+            get { return (double)GetValue(ProgressProperty); }
+            set { SetValue(ProgressProperty, value); }
+        }
+
+        private static void OnProgressPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var progressBar = (CircleProgressBar)bindable;
+            progressBar.InvalidateSurface();
+        }
+
+        // Определение свойства цвета прогресса
+        public static readonly BindableProperty ProgressColorProperty =
+            BindableProperty.Create(nameof(ProgressColor), typeof(Color), typeof(CircleProgressBar), Color.Default);
+
+        public Color ProgressColor
+        {
+            get { return (Color)GetValue(ProgressColorProperty); }
+            set { SetValue(ProgressColorProperty, value); }
+        }
+
+        protected override void OnPaintSurface(SKPaintSurfaceEventArgs args)
+        {
+            base.OnPaintSurface(args);
+
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+            SKImageInfo info = args.Info;
+
+            canvas.Clear();
+
+            // Рисуем круговой фон
+            float strokeWidth = 10;
+            float radius = Math.Min(info.Width, info.Height) / 2;
+            float centerX = info.Width / 2;
+            float centerY = info.Height / 2;
+
+            SKPaint backgroundPaint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Color.Gray.ToSKColor(),
+                StrokeWidth = strokeWidth
+            };
+
+            canvas.DrawCircle(centerX, centerY, radius, backgroundPaint);
+
+            // Рисуем прогресс
+            SKPaint progressPaint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = ProgressColor.ToSKColor(),
+                StrokeWidth = strokeWidth,
+                StrokeCap = SKStrokeCap.Round
+            };
+
+            float sweepAngle = (float)(Progress * 360);
+            canvas.DrawArc(new SKRect(centerX - radius, centerY - radius, centerX + radius, centerY + radius), -90, sweepAngle, false, progressPaint);
+        }
+    }
+}
---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.TizenSpecific;
+
+namespace App1
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private bool isRunning = false;
+        private int totalTime = 60; // Время в секундах
+        private int currentTime;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void StartButton_Clicked(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+
+                currentTime = totalTime;
+                progressBar.Progress = 1;
+                timerLabel.Text = FormatTime(currentTime);
+
+                while (currentTime > 0 && isRunning)
+                {
+                    await Task.Delay(1000);
+                    currentTime--;
+                    progressBar.Progress = (double)currentTime / totalTime;
+                    timerLabel.Text = FormatTime(currentTime);
+                }
+
+                if (isRunning)
+                {
+                    // Таймер завершился
+                    await DisplayAlert("Таймер", "Время истекло!", "OK");
+                }
+
+                isRunning = false;
+            }
+        }
+
+        private void StopButton_Clicked(object sender, EventArgs e)
+        {
+            isRunning = false;
+        }
+
+        private string FormatTime(int timeInSeconds)
+        {
+            TimeSpan timeSpan = TimeSpan.FromSeconds(timeInSeconds);
+            return string.Format("{0:00}:{1:00}", timeSpan.Minutes, timeSpan.Seconds);
+        }
+    }
+}
